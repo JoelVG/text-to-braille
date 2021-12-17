b031cb7c637bdceb67756deaa289c5cdd92d7bbf
JoelVG <cgjavg@gmail.com>
2021-12-17 16:41:10 -0400

    Generate react basic structure.

---
+++ braille/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <p>Holii joel</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
