af82089b4e761911d840fb677a394ac990812aa4
Joel VG <cgjavg@gmail.com>
2022-02-07 10:51:03 -0400

    Refactor API creation

--- braille/braille/settings.py
+++ braille/braille/settings.py
@@ -36,10 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'textToBraille.apps.TexttobrailleConfig',
-    'frontend.apps.FrontendConfig',
-    'rest_framework',
-    'corsheaders',
+    'textToBraille',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- braille/textToBraille/views.py
+++ braille/textToBraille/views.py
@@ -1,28 +1,15 @@
-from django.shortcuts import render
-import rest_framework
-from rest_framework import serializers, viewsets
-from rest_framework.views import APIView
-from .models import Translation
-from rest_framework.response import Response
-from .serializer import TranslationSerializer
-from .py_braille import convert_text
+from rest_framework import mixins, viewsets
+from textToBraille.models import Translation
+from textToBraille.serializer import TranslationSerializer
 
-#SECOND OPTION
-# class TranslationView(viewsets.ModelViewSet):
-#     serializer_class = TranslationSerializer
-#     queryset = Translation.objects.all()
 
-class TranslationView(APIView):
+class TranslationViewSet(mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet):
     
     serializer_class = TranslationSerializer
-    
-    def get(self, request):
-        transcription = [ {"text": detail.text,"braille_translation": detail.detail} 
-        for detail in Translation.objects.all()]
-        return Response(transcription)
-    
-    
-    def post(self, request):
-        data = request.data
-        return Response(convert_text(data['text']))
-        
+    queryset = Translation.objects.all()
+    pass
